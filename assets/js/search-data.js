c6c1d7e771c34804267265fd0ad8d7812b7c9829
lelel-hue <132806922+lelel-hue@users.noreply.github.com>
2025-11-07 03:54:44 +0000

    Deploying to gh-pages from @ lelel-hue/lelel-hue.github.io@cd6ff7605fe5ddc4528a2b99b9c0e116d7252fbe 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -398,6 +398,11 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
+            },},{id: "news-a-long-announcement-with-details",
+          title: 'A long announcement with details',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_2/";
             },},{id: "news-i-received-the-first-class-outstanding-student-scholarship-at-sustech-top-3-smile",
           title: 'I received the First-Class Outstanding Student Scholarship at SUSTech (top 3%). :smile:',
           description: "",
