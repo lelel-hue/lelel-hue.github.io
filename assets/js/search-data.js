9f4fbba6cb71bf522869d710addff01c9c009223
lelel-hue <132806922+lelel-hue@users.noreply.github.com>
2025-11-08 03:21:05 +0000

    Deploying to gh-pages from @ lelel-hue/lelel-hue.github.io@75376486c725fb9f28ab712ac47a101db9279469 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -407,9 +407,9 @@ ninja.data = [{
           section: "News",},{id: "news-our-paper-gaussmedact-was-accepted-by-aaai-2026-sparkles",
           title: 'Our paper GaussMedAct was accepted by AAAI 2026. :sparkles:',
           description: "",
-          section: "News",},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+          section: "News",},{id: "projects-content-aware-image-resizing-with-seam-carving",
+          title: 'Content-Aware Image Resizing with Seam Carving',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
